6264c55178a92da4f6309418fd738b967290a0bd
Dandan MENG <mengdandanno1@163.com>
2015-10-13 18:04:59 +0800

    Test 1: send a request, but Internet permission is not declared in manifest Test 1 result: throw SecurityException when sending request, result in crash

--- app/src/main/java/com/ddmeng/androidmruntimepermissionsample/MainActivity.java
+++ app/src/main/java/com/ddmeng/androidmruntimepermissionsample/MainActivity.java
@@ -1,17 +1,30 @@
 package com.ddmeng.androidmruntimepermissionsample;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TEST_URL = "http://www.cnblogs.com/mengdd/";
+    private static final String DEBUG_TAG = "PermissionTest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +44,65 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.send_request)
     void sendRequest() {
-        //TODO send a network request
+
+
+        new AsyncTask<String, Void, String>() {
+            @Override
+            protected String doInBackground(String... params) {
+                String input = params[0];
+                InputStream is = null;
+                // Only display the first 500 characters of the retrieved
+                // web page content.
+                int len = 500;
+
+                try {
+                    URL url = new URL(input);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setReadTimeout(10000 /* milliseconds */);
+                    conn.setConnectTimeout(15000 /* milliseconds */);
+                    conn.setRequestMethod("GET");
+                    conn.setDoInput(true);
+                    // Starts the query
+                    conn.connect();
+                    int response = conn.getResponseCode();
+                    Log.d(DEBUG_TAG, "The response is: " + response);
+                    is = conn.getInputStream();
+
+                    // Convert the InputStream into a string
+                    String contentAsString = readIt(is, len);
+                    return contentAsString;
+
+                    // Makes sure that the InputStream is closed after the app is
+                    // finished using it.
+                } catch (IOException e) {
+                    Log.e(DEBUG_TAG, "Catch IOException: " + e);
+                } finally {
+                    if (is != null) {
+                        try {
+                            is.close();
+                        } catch (IOException e) {
+                            Log.e(DEBUG_TAG, "Catch IOException when close: " + e);
+                        }
+                    }
+                }
+                return null;
+            }
+
+
+            @Override
+            protected void onPostExecute(String result) {
+                Log.d(DEBUG_TAG, "The result is: " + result);
+            }
+        }.execute(TEST_URL);
+    }
+
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
     }
 
     @Override
