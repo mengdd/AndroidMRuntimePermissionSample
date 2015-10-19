e5cb8434520b98059dcaaf31bc40aab24e485c96
Dandan MENG <mengdandanno1@163.com>
2015-10-19 14:14:39 +0800

    add implementation of Requesting Permissions at Run Time according to developer doc

--- app/src/main/java/com/ddmeng/androidmruntimepermissionsample/MainActivity.java
+++ app/src/main/java/com/ddmeng/androidmruntimepermissionsample/MainActivity.java
@@ -1,9 +1,14 @@
 package com.ddmeng.androidmruntimepermissionsample;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TEST_URL = "http://www.cnblogs.com/mengdd/";
     private static final String DEBUG_TAG = "PermissionTest";
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +46,66 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.read_contacts)
     void readContacts() {
-        ContactsUtils.readPhoneContacts(this);
+        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS)) {
+            //has permission, do operation directly
+            ContactsUtils.readPhoneContacts(this);
+            Log.i(DEBUG_TAG, "user has the permission already!");
+        } else {
+            //do not have permission
+            Log.i(DEBUG_TAG, "user do not have this permission!");
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Log.i(DEBUG_TAG, "we should explain why we need this permission!");
+                //TODO show a explanation dialog
+            } else {
+
+                // No explanation needed, we can request the permission.
+                Log.i(DEBUG_TAG, "==request the permission==");
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    ContactsUtils.readPhoneContacts(this);
+                    Log.i(DEBUG_TAG, "user granted the permission!");
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Log.i(DEBUG_TAG, "user denied the permission!");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
